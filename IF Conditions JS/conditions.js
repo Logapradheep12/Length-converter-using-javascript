f38cbfb79abce2b8feab87c3c9f62702f004c657
Logapradheep12 <leoprasath7@gmail.com>
2025-03-22 14:57:37 +0530

    feat : conditions

---
+++ IF Conditions JS/conditions.js
@@ -0,0 +1,50 @@
+// syntax for if conditions
+
+let Password = true
+
+if(Password)
+    console.log("You Can Proceed Now...")
+else
+    console.log("Please Check Once a Password...")
+
+console.log("See you Again.... Bye :) ")
+
+
+let Password_false = false
+
+if(Password_false)
+    console.log("You Can Proceed Now...")
+else
+    console.log("Please Check Once a Password...")
+
+console.log("See you Again.... Bye :) ")
+
+// Comaparison and Logical Operator
+
+age = 55
+gender =  'male'
+
+if(age > 60 && gender === 'male')
+    console.log("Eligible to Retire...")
+
+else
+    console.log("Go and do your work First.....")
+
+console.log("Improve your knowledge :)")
+
+// Max of two Numbers
+
+let a = 50
+let b = 51
+let max
+
+if (a>b)
+    max = a
+else
+    max = b
+
+max = a>b?a:b // ternary operator
+
+console.log(max)
+
+console.log(" Got Mximum Number ")
